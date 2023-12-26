6ee010b5cacad499d6da0c67387398d7ecee3bad
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-26 19:53:28 +0100

    [add] fetch for information

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = 3003;
 app.use(express.json());
 app.use(cors());
 
+SYS_ManageData_Port = 3002;
+
 function sendEmail(content) {
 	const transporter = nodemailer.createTransport({
 		service: 'gmail',
@@ -33,8 +35,24 @@ function sendEmail(content) {
 	});
 }
 
+async function getInfo(info) {
+	fetch(`http://localhost:${SYS_SendMail_Port}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			token: info.token,
+		}),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			sendEmail(data);
+		});
+}
+
 app.post('', (req, res) => {
-	sendEmail(req.body);
+	getInfo(req.body);
 });
 
 app.listen(port, () => console.log(`The SYS-SendMail server runs on port ${port}`));
